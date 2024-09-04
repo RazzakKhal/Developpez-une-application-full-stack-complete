8aa09409a5645f661ae8282a30c33a1b948cf5a3
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-09-05 01:40:42 +0200

    chore: back ok

--- back/src/main/java/com/openclassrooms/mddapi/controllers/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/UserController.java
@@ -1,4 +1,25 @@
 package com.openclassrooms.mddapi.controllers;
 
+import com.openclassrooms.mddapi.dtos.fromView.RegisterUserDto;
+import com.openclassrooms.mddapi.dtos.toView.UserDto;
+import com.openclassrooms.mddapi.models.User;
+import com.openclassrooms.mddapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PutMapping("")
+    Map<String,String> updateAnUser(Authentication authentication, @RequestBody RegisterUserDto updateUserDto){
+        return userService.updateAnUser((User) authentication.getPrincipal(), updateUserDto);
+    }
 }
--- back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
@@ -1,2 +1,12 @@
-package com.openclassrooms.mddapi.services;public interface UserService {
+package com.openclassrooms.mddapi.services;
+
+import com.openclassrooms.mddapi.dtos.fromView.RegisterUserDto;
+import com.openclassrooms.mddapi.dtos.toView.UserDto;
+import com.openclassrooms.mddapi.models.User;
+
+import java.security.Principal;
+import java.util.Map;
+
+public interface UserService {
+    Map<String, String> updateAnUser(User principal, RegisterUserDto updateUserDto);
 }
