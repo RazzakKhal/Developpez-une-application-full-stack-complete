c5219cd53e347680897a019fe58a6008ae24498c
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-09-11 02:26:50 +0200

    chore: add of error notif front side

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { mergeMap } from 'rxjs';
+import { snackBarFailConfiguration, SnackBarMessageEnum } from 'src/app/shared/helpers/material.helper';
 import { Article } from 'src/app/shared/models/Article';
 import { User } from 'src/app/shared/models/User';
 import { ArticleService } from 'src/app/shared/services/article.service';
@@ -14,10 +16,12 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class ArticlesComponent implements OnInit {
 
-  articles : Article[] = []
-  user! : User;
+  articles: Article[] = []
+  user!: User;
 
-  constructor(private articleService : ArticleService, private authService : AuthService, private userService : UserService, private router : Router) { }
+  constructor(private articleService: ArticleService, private authService: AuthService, private userService: UserService, private router: Router,
+    private snackBar : MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.getUserAndArticles()
@@ -28,36 +32,38 @@ export class ArticlesComponent implements OnInit {
 
   getUserAndArticles() {
     if (this.authService.isAuthenticate()) {
-    this.userService.getUser().pipe(
-      mergeMap((item : User) => {
-        this.user = item
-       return this.articleService.getUserSubscribedArticles(this.user.id)
-      })
-    ).subscribe((articles : Article[]) => {
-      this.sortArticlesByDateAsc(articles);
-      console.log('articles',articles)
-        this.articles = articles;
+      this.userService.getUser().pipe(
+        mergeMap((item: User) => {
+          this.user = item
+          return this.articleService.getUserSubscribedArticles(this.user.id)
+        })
+      ).subscribe({
+        next: (articles: Article[]) => {
+          this.sortArticlesByDateAsc(articles);
+          this.articles = articles;
+        },
+        error: () => {snackBarFailConfiguration(this.snackBar, SnackBarMessageEnum.FAIL_LOAD_ARTICLES);}
       })
     }
   }
 
-  redirectToTheArticle(article : Article){
+  redirectToTheArticle(article: Article) {
     this.router.navigateByUrl(`/articles/${article.id}`)
   }
 
-  sortArticlesByDateAsc(articles : Article[]){
-    articles.sort((item : Article, item2 : Article) => {
+  sortArticlesByDateAsc(articles: Article[]) {
+    articles.sort((item: Article, item2: Article) => {
       return (new Date(item.createdAt)).getTime() - (new Date(item2.createdAt)).getTime()
     })
   }
 
-  sortArticlesByDateDsc(articles : Article[]){
-    articles.sort((item : Article, item2 : Article) => {
+  sortArticlesByDateDsc(articles: Article[]) {
+    articles.sort((item: Article, item2: Article) => {
       return (new Date(item2.createdAt)).getTime() - (new Date(item.createdAt)).getTime()
     })
   }
 
-  descSorting(){
+  descSorting() {
     this.sortArticlesByDateDsc(this.articles)
   }
 }
--- front/src/app/shared/components/theme/theme.component.ts
+++ front/src/app/shared/components/theme/theme.component.ts
@@ -4,6 +4,8 @@ import { User } from '../../models/User';
 import { Subscription } from '../../models/Subscription';
 import { SubscriptionCreation } from '../../entities/Subscription';
 import { SubscriptionService } from '../../services/subscription.service';
+import { snackBarFailConfiguration, SnackBarMessageEnum } from '../../helpers/material.helper';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-theme',
@@ -17,7 +19,7 @@ export class ThemeComponent implements OnInit {
   @Output() userChange: EventEmitter<User> = new EventEmitter();
 
 
-  constructor(private subscriptionService : SubscriptionService) { }
+  constructor(private subscriptionService : SubscriptionService, private snackBar : MatSnackBar) { }
 
   ngOnInit(): void {
   }
@@ -33,14 +35,18 @@ export class ThemeComponent implements OnInit {
     const subscription = new SubscriptionCreation(this.user.id, theme.id);
     this.subscriptionService.createSubscription(subscription).subscribe({
       next : (res : User) => {this.userChange.emit(res)},
-      error : () => {}
+      error : () => {
+        snackBarFailConfiguration(this.snackBar, SnackBarMessageEnum.FAIL_SUBSCRIBE)
+      }
     })
   }
 
   onUnsubscribe(theme : Theme){
     this.subscriptionService.deleteSubscription(this.user.id, theme.id).subscribe({
       next : (res : User) => {this.userChange.emit(res)},
-      error : () => {}
+      error : () => {
+        snackBarFailConfiguration(this.snackBar, SnackBarMessageEnum.FAIL_UNSUBSCRIBE)
+      }
     })
   }
 
